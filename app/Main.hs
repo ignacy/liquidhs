408c244e47d47ec62ca9e220ea4475c1d1c49369
Ignacy Moryc <imoryc@gmail.com>
2019-06-25 09:01:09 +0200

    Prints awesome error messages

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,18 @@ module Main where
 
 import Liquid
 import Text.Megaparsec
+import Data.Void
+
+
+printParsingResults :: Either (ParseErrorBundle String Data.Void.Void) LiquidObject -> String
+printParsingResults eitherLiquidOrError =
+    case (eitherLiquidOrError) of
+        Left errorBundle -> errorBundlePretty errorBundle
+        Right success -> "success"
 
 main :: IO ()
 main = do
   input <- getContents
-  parseTest whileParser input
+  --  parseTest whileParser input
+  putStr $ printParsingResults(parse whileParser "" input)
+ 

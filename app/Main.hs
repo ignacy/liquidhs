70922594b11f53ca8e79655f5ceab90c12def7e5
Ignacy Moryc <imoryc@gmail.com>
2019-07-18 20:59:46 +0200

    Working with a simple web server

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@
 
 module Main where
 
+import Snap.Http.Server
+import Snap.Util.FileServe
+import Snap.Core
 import Liquid
 import Text.Megaparsec
 import Data.Void
@@ -16,8 +19,24 @@ printParsingResults eitherLiquidOrError =
         Left errorBundle -> errorBundlePretty errorBundle
         Right success -> show success
 
+-- main = do
+--   input <- getContents
+--   --  parseTest whileParser input
+--   putStr $ printParsingResults(parse whileParser "" (T.pack input))
+
 main :: IO ()
-main = do
-  input <- getContents
-  --  parseTest whileParser input
-  putStr $ printParsingResults(parse whileParser "" (T.pack input))
+main = quickHttpServe site
+
+site :: Snap ()
+site =
+    ifTop (writeBS "hello world") <|>
+    route [ ("foo", writeBS "bar")
+          , ("echo/:echoparam", echoHandler)
+          ] <|>
+    dir "static" (serveDirectory ".")
+
+echoHandler :: Snap ()
+echoHandler = do
+    param <- getParam "echoparam"
+    maybe (writeBS "must specify echo/param in URL")
+          writeBS param

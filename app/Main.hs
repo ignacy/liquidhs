485e083141b7a0682b69383fc2de3c578c2132b0
Ignacy Moryc <imoryc@gmail.com>
2019-07-19 20:16:50 +0200

    Remove snap and all web stuff

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,6 @@
 
 module Main where
 
-import Snap.Http.Server
-import Snap.Util.FileServe
-import Snap.Core
 import Liquid
 import Text.Megaparsec
 import Data.Void
@@ -18,25 +15,3 @@ printParsingResults eitherLiquidOrError =
     case (eitherLiquidOrError) of
         Left errorBundle -> errorBundlePretty errorBundle
         Right success -> show success
-
--- main = do
---   input <- getContents
---   --  parseTest whileParser input
---   putStr $ printParsingResults(parse whileParser "" (T.pack input))
-
-main :: IO ()
-main = quickHttpServe site
-
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
-
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param

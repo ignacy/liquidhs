6b73c0ee006d1260224c324c59696292eee19b92
Ignacy Moryc <imoryc@gmail.com>
2020-07-27 18:42:26 +0200

    Print multiple page results

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
 
 module Main where
 
-import Liquid
-import Compiler (compile)
-import Params
-import DB (getPage)
-import Text.Megaparsec
-import Data.Void
-import Hasql.Session
+import           Compiler         (compile)
+import           Data.Void
+import           DB               (getPage)
+import           Hasql.Session
+import           Liquid
+import           Params
+import           Text.Megaparsec
 
 
-import qualified Data.Text as T
+import qualified Data.Text        as T
 import qualified Hasql.Connection as Connection
 
 default (T.Text)
@@ -21,7 +21,7 @@ printParsingResults :: Either (ParseErrorBundle T.Text Data.Void.Void) LiquidObj
 printParsingResults = extract
   where
     extract (Left errorBundle) = errorBundlePretty errorBundle
-    extract (Right success) = show success
+    extract (Right success)    = show success
 
 -- work :: Params -> IO ()
 -- work params = do
@@ -34,11 +34,15 @@ printParsingResults = extract
 main :: IO ()
 main = do
   Right connection <- Connection.acquire connectionSettings
-  result <- Hasql.Session.run (getPage 52) connection
-  case result of
-    Left qe -> putStr $ show qe
-    Right r -> case r of
-      Nothing -> putStr "Nothing found"
-      Just s -> putStr $ printParsingResults(Text.Megaparsec.parse whileParser "" s)
+  --  result <- Hasql.Session.run (getPage 52) connection
+  mapM_ (processPage connection) [10..40]
   where
+    processPage :: Connection.Connection -> Int -> IO ()
+    processPage connection pageId = do
+      result <- Hasql.Session.run (getPage (fromIntegral pageId)) connection
+      case result of
+        Left qe -> putStr $ show qe
+        Right r -> case r of
+          Nothing -> putStrLn $ "Page with ID=" ++ (show pageId) ++ " not found"
+          Just s -> putStrLn $ "---> For Page with ID=" ++ (show pageId) ++ printParsingResults(Text.Megaparsec.parse whileParser "" s)
     connectionSettings = Connection.settings "localhost" 5432 "nearme" "nearme" "nearme_development"
